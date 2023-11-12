c1cdbe39f930c77e03b2a2b600c66d75de7083b9
selin pir <113732977+selinpir@users.noreply.github.com>
2023-11-12 10:28:06 +0300

    .

---
+++ taki dukkani/hafta 6 gorsel-prog/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace hafta_6_gorsel_prog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(48, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ÜRÜN İSMİ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(48, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ÜRÜN FİYATI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(48, 401);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ÜRÜN STOĞU";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 230);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(205, 314);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 25);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(205, 402);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(117, 25);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSalmon;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(36, 484);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(296, 64);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "K A Y I T   E K L E";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(370, 224);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(409, 232);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(36, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(743, 173);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkSalmon;
+            this.button3.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(431, 484);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(296, 64);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "G Ö R Ü N T Ü L E";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(800, 585);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Leda Shop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ taki dukkani/hafta 6 gorsel-prog/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace hafta_6_gorsel_prog
+{
+    public partial class Form1 : Form
+    {
+        ArrayList urunA = new ArrayList();
+
+        ArrayList urunF = new ArrayList();
+
+        ArrayList urunS = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "") MessageBox.Show("Lütfen ürün ismi Giriniz");
+            else urunA.Add(textBox1.Text);
+
+            textBox1.Clear();
+            textBox1.Focus();
+            listBox1.Items.Clear();
+            listele();
+
+
+            if (textBox2.Text == "") MessageBox.Show("Lütfen ürün fiyatı Giriniz");
+            else urunF.Add(textBox2.Text);
+
+            textBox2.Clear();
+            textBox2.Focus();
+            listBox1.Items.Clear();
+            listele();
+
+            if (textBox3.Text == "") MessageBox.Show("Lütfen ürün stoğu Giriniz");
+            else urunS.Add(textBox3.Text);
+
+            textBox3.Clear();
+            textBox3.Focus();
+            listBox1.Items.Clear();
+            listele();
+
+        }
+
+        void listele()
+        {
+
+            for (int i = 0; i < urunA.Count; i++)
+            {
+                listBox1.Items.Add(urunA[i].ToString());
+          
+            }
+            for (int i = 0; i < urunF.Count; i++)
+            {
+                listBox1.Items.Add(urunF[i].ToString());
+
+            }
+
+            for (int i = 0; i < urunS.Count; i++)
+            {
+                listBox1.Items.Add(urunS[i].ToString());
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("Listede herhangi bir ürün bulunmamaktadır.");
+                return;
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            // En az ve fazla sayıda stokta bulunan ürünler
+            int minStok = Convert.ToInt32(urunS[0]);
+            int maxStok = Convert.ToInt32(urunS[0]);
+            string minStokUrun = urunA[0].ToString();
+            string maxStokUrun = urunA[0].ToString();
+
+            // En ucuz ve en pahalı fiyata satılan ürünler
+            double minFiyat = Convert.ToDouble(urunF[0]);
+            double maxFiyat = Convert.ToDouble(urunF[0]);
+            string minFiyatUrun = urunA[0].ToString();
+            string maxFiyatUrun = urunA[0].ToString();
+
+
+            // Toplam kayıtlı ürün sayısı
+            int toplamUrunSayisi = urunA.Count;
+
+            // Ürünlerin fiyat ortalaması
+            double toplamFiyat = 0;
+
+            for (int i = 0; i < urunS.Count; i++)
+            {
+                int stok = Convert.ToInt32(urunS[i]);
+                double fiyat = Convert.ToDouble(urunF[i]);
+
+                // En az ve fazla sayıda stokta bulunan ürünler
+                if (stok < minStok)
+                {
+                    minStok = stok;
+                    minStokUrun = urunA[i].ToString();
+                }
+
+                if (stok > maxStok)
+                {
+                    maxStok = stok;
+                    maxStokUrun = urunA[i].ToString();
+                }
+
+                // En ucuz ve en pahalı fiyata satılan ürünler
+                if (fiyat < minFiyat)
+                {
+                    minFiyat = fiyat;
+                    minFiyatUrun = urunA[i].ToString();
+                }
+
+                if (fiyat > maxFiyat)
+                {
+                    maxFiyat = fiyat;
+                    maxFiyatUrun = urunA[i].ToString();
+                }
+
+                // Toplam fiyatı hesapla
+                toplamFiyat += fiyat;
+            }
+
+            // Ürünlerin fiyat ortalaması
+            double fiyatOrtalamasi = toplamFiyat / urunA.Count;
+
+            listBox1.Items.Add("En az stokta bulunan ürün: " + minStokUrun + " (" + minStok + " adet)");
+            listBox1.Items.Add("En fazla stokta bulunan ürün: " + maxStokUrun + " (" + maxStok + " adet)");
+            listBox1.Items.Add("");
+            listBox1.Items.Add("En ucuz fiyatlı ürün: " + minFiyatUrun + " (" + minFiyat.ToString("C") + ")");
+            listBox1.Items.Add("En pahalı fiyatlı ürün: " + maxFiyatUrun + " (" + maxFiyat.ToString("C") + ")");
+            listBox1.Items.Add("");
+            listBox1.Items.Add("Toplam kayıtlı ürün sayısı: " + toplamUrunSayisi);
+            listBox1.Items.Add("");
+            listBox1.Items.Add("Ürünlerin fiyat ortalaması: " + fiyatOrtalamasi.ToString("C"));
+
+        }
+    }
+}
